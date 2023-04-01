a77598780600b10934e8f7165bc1935f14648dd7
Martin Clementz <martcl@stud.ntnu.no>
2023-04-01 14:24:18 +0200

    added ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,46 +1,37 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 import image from '@astrojs/image';
 import compress from 'astro-compress';
-
 import { SITE } from './src/config.ts';
-
+import netlify from "@astrojs/netlify/functions";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+// https://astro.build/config
 export default defineConfig({
   site: SITE.origin,
   base: SITE.basePathname,
   trailingSlash: SITE.trailingSlash ? 'always' : 'never',
-
   output: 'static',
-
-  integrations: [
-    sitemap(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-    compress({
-      css: true,
-      html: {
-        removeAttributeQuotes: false,
-      },
-      img: false,
-      js: true,
-      svg: false,
-
-      logger: 1,
-    }),
-  ],
-
+  integrations: [sitemap(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), compress({
+    css: true,
+    html: {
+      removeAttributeQuotes: false
+    },
+    img: false,
+    js: true,
+    svg: false,
+    logger: 1
+  })],
   vite: {
     resolve: {
       alias: {
-        '~': path.resolve(__dirname, './src'),
-      },
-    },
+        '~': path.resolve(__dirname, './src')
+      }
+    }
   },
+  adapter: netlify()
 });
