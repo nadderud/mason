e49a85992c0eec2eac6308725500b145f66fe6ae
Martin Clementz <martcl@stud.ntnu.no>
2023-04-13 19:03:40 +0200

    small fix

--- src/utils/permalinks.ts
+++ src/utils/permalinks.ts
@@ -5,9 +5,9 @@ export const trimSlash = (s: string) => trim(trim(s, "/"));
 
 export const getCanonical = (path = ""): string | URL => {
   const url = String(new URL(path, SITE.origin));
-  if (SITE.trailingSlash == false && path && url.endsWith("/")) {
+  if (path && url.endsWith("/")) {
     return url.slice(0, -1);
-  } else if (SITE.trailingSlash == true && path && !url.endsWith("/")) {
+  } else if (path && !url.endsWith("/")) {
     return url + "/";
   }
   return url;
